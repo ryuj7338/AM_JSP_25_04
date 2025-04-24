8e900901d44c87907a611c7dee2e30a541dbc5b4
ryuj7338 <ryuj7338@gmail.com>
2025-04-24 16:02:02 +0900

    getParmeter 사용

--- src/main/java/com/KoreaIT/java/AM_jsp/PrintDanServlet.java
+++ src/main/java/com/KoreaIT/java/AM_jsp/PrintDanServlet.java
@@ -16,13 +16,18 @@ public class PrintDanServlet extends HttpServlet {
 		
 		response.setContentType("text/html;charset=UTF-8");
 		
-		response.getWriter().append("== 8단 ==<br>");
+		
 		
 //		response.getWriter().append("8 * 1 = 8\n");
 //		response.getWriter().append(String.format("%d * %d = %d<br>", 8, 1, 8));
 //		<br> : 줄바꿈
 		
-		int dan = 8;
+		
+	
+		
+		int dan = Integer.parseInt(request.getParameter("dan"));
+		
+		response.getWriter().append(String.format("== %d단 ==<br>", dan));
 		
 		for (int i = 1; i <= 9; i++ ) {
 			response.getWriter().append(String.format("%d * %d = %d<br>", dan, i, dan * i));
