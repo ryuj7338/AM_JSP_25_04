640474a908caa46a23e6904dbcbf2232b6066cac
ryuj7338 <ryuj7338@gmail.com>
2025-04-27 04:53:25 +0900

    상세페이지, a태그 활용

--- src/main/java/com/KoreaIT/java/AM_jsp/ArticleDetailServlet.java
+++ src/main/java/com/KoreaIT/java/AM_jsp/ArticleDetailServlet.java
@@ -13,7 +13,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/article/datail")
+@WebServlet("/article/detail")
 public class ArticleDetailServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
